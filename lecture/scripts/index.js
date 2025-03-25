b1682ddb355d5203c9245c9b6da74f40d47d9629
MarkSian <merksianipar@gmail.com>
2025-03-25 09:39:33 -0600

    Completed LAB
    will be used in the weighted lab

---
+++ lecture/scripts/index.js
@@ -0,0 +1,27 @@
+// Part 1
+const mainEl = document.querySelector("main");
+mainEl.style.backgroundColor = "var(--main-bg)";
+mainEl.innerHTML = "<h1>DOM Manipulation</h1>";
+mainEl.classList.add("flex-ctr");
+
+// Part 2
+const topMenuEl = document.getElementById("top-menu");
+topMenuEl.style.height = "100%";
+topMenuEl.style.backgroundColor = "var(--top-menu-bg)";
+topMenuEl.classList.add("flex-around");
+
+// Part 3
+var menuLinks = [
+    { text: "about", href: "/about" },
+    { text: "catalog", href: "/catalog" },
+    { text: "orders", href: "/orders" },
+    { text: "account", href: "/account" },
+];
+
+menuLinks.forEach(link => {
+    let a = document.createElement("a");
+    a.href = link.href;
+    a.textContent = link.text;
+    topMenuEl.appendChild(a);
+});
+// Part 4: Save code for next lab.
